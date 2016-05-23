39f450e5e7f1a11a2439d63e8f50984ea847b6d2
Aaron France <aaron.l.france@gmail.com>
2016-05-23 22:04:20 +0200

    Add get_buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 pub mod emacs_module;
 
 extern crate libc;
-use emacs_module::{emacs_subr, emacs_env, emacs_value, Struct_emacs_value_tag, Struct_emacs_runtime,
-                   Struct_emacs_env_25};
+use emacs_module::{emacs_subr, emacs_env, emacs_value, Struct_emacs_value_tag,
+                   Struct_emacs_runtime, Struct_emacs_env_25};
 use std::ffi::CString;
 use std::os::raw;
 
@@ -22,6 +22,14 @@ pub extern "C" fn make_function(env: *mut emacs_env,
                                 -> *mut Struct_emacs_value_tag {
     let doc = CString::new(doc).unwrap().as_ptr();
     unsafe { (*env).make_function.unwrap()(env, min_args, max_args, f, doc, user_ptr) }
+}
+
+pub extern "C" fn make_emacs_string<T: Into<Vec<u8>>>(env: *mut emacs_env,
+                                                      string: T)
+                                                      -> *mut Struct_emacs_value_tag {
+    let c_string = CString::new(string).unwrap().as_ptr();
+    let strlen = unsafe { libc::strlen(c_string) as i64 };
+    unsafe { (*env).make_string.unwrap()(env, c_string, strlen) }
 }
 
 #[no_mangle]
@@ -48,4 +56,12 @@ pub extern "C" fn provide(env: *mut emacs_env, feature: String) {
     let provide = find_function(env, "provide".to_string());
     let args = [feat].as_mut_ptr();
     unsafe { (*env).funcall.unwrap()(env, provide, 1, args) };
+}
+
+
+#[no_mangle]
+pub extern "C" fn get_buffer(env: *mut emacs_env, buffer: String) -> *mut Struct_emacs_value_tag {
+    let get_buffer = find_function(env, "get-buffer".to_string());
+    let args = [make_emacs_string(env, buffer)].as_mut_ptr();
+    unsafe { (*env).funcall.unwrap()(env, get_buffer, 1, args) }
 }
