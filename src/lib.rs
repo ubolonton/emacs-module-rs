8f195b04e31ec3a6480de1da15f8f2a0f1f70ba9
Aaron France <aaron.l.france@gmail.com>
2016-05-23 02:25:10 +0200

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,46 @@
 pub mod emacs_module;
 
 extern crate libc;
-use emacs_module::{emacs_env, emacs_value, Struct_emacs_value_tag, Struct_emacs_runtime, Struct_emacs_env_25};
+use emacs_module::{emacs_env, emacs_value, Struct_emacs_value_tag, Struct_emacs_runtime,
+                   Struct_emacs_env_25};
 use std::ffi::CString;
 use std::os::raw;
 
-pub type EmacsFunction = Option<unsafe extern "C" fn(*mut Struct_emacs_env_25, i64, *mut *mut Struct_emacs_value_tag, *mut raw::c_void) -> *mut Struct_emacs_value_tag>;
+pub type EmacsFunction = Option<unsafe extern "C" fn(*mut Struct_emacs_env_25,
+                                                     i64,
+                                                     *mut *mut Struct_emacs_value_tag,
+                                                     *mut raw::c_void)
+                                                     -> *mut Struct_emacs_value_tag>;
 
 #[no_mangle]
-pub extern fn find_function(env: * mut emacs_env, name: String) -> * mut Struct_emacs_value_tag {
+pub extern "C" fn find_function(env: *mut emacs_env, name: String) -> *mut Struct_emacs_value_tag {
     unsafe { (*env).intern.unwrap()(env, CString::new(name).unwrap().as_ptr()) }
 }
 
 #[no_mangle]
-pub extern fn make_function(env: * mut emacs_env, min_args: i64, max_args: i64, f: EmacsFunction, doc: String, user_ptr: *mut raw::c_void) -> *mut Struct_emacs_value_tag {
+pub extern "C" fn make_function(env: *mut emacs_env,
+                                min_args: i64,
+                                max_args: i64,
+                                f: EmacsFunction,
+                                doc: String,
+                                user_ptr: *mut raw::c_void)
+                                -> *mut Struct_emacs_value_tag {
     let doc = CString::new(doc).unwrap().as_ptr();
     unsafe { (*env).make_function.unwrap()(env, min_args, max_args, f, doc, user_ptr) }
 }
 
 #[no_mangle]
-pub extern fn get_environment(ert: * mut Struct_emacs_runtime) -> * mut Struct_emacs_env_25 {
+pub extern "C" fn get_environment(ert: *mut Struct_emacs_runtime) -> *mut Struct_emacs_env_25 {
     unsafe { (*ert).get_environment.unwrap()(ert) }
 }
 
 #[no_mangle]
-pub extern fn intern_symbol(env: * mut emacs_env, name: String) -> * mut Struct_emacs_value_tag {
+pub extern "C" fn intern_symbol(env: *mut emacs_env, name: String) -> *mut Struct_emacs_value_tag {
     unsafe { (*env).intern.unwrap()(env, CString::new(name).unwrap().as_ptr()) }
 }
 
 #[no_mangle]
-pub extern fn bind_function(env: * mut emacs_env, name: String, sfun: emacs_value) {
+pub extern "C" fn bind_function(env: *mut emacs_env, name: String, sfun: emacs_value) {
     let qfset = find_function(env, "fset".to_string());
     let qsym = intern_symbol(env, name);
     let args = [qsym, sfun].as_mut_ptr();
@@ -37,7 +48,7 @@ pub extern fn bind_function(env: * mut emacs_env, name: String, sfun: emacs_valu
 }
 
 #[no_mangle]
-pub extern fn provide(env: * mut emacs_env, feature: String) {
+pub extern "C" fn provide(env: *mut emacs_env, feature: String) {
     let feat = unsafe { (*env).intern.unwrap()(env, CString::new(feature).unwrap().as_ptr()) };
     let provide = find_function(env, "provide".to_string());
     let args = [feat].as_mut_ptr();
