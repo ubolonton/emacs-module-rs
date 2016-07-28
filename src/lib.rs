95e55cfdfae0c6e4e995d7fb4b8e7fe5328e3e30
Joey Ezechiëls <joey.ezechiels@gmail.com>
2016-07-28 04:36:30 +0200

    Make `unsafe` blocks more readable

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,10 @@ use std::os::raw;
 #[no_mangle]
 pub extern "C" fn find_function(env: *mut EmacsEnv, name: String)
                                 -> *mut EmacsVal {
-    unsafe { (*env).intern.unwrap()(env, CString::new(name).unwrap().as_ptr()) }
+    unsafe {
+        let intern = (*env).intern.unwrap();
+        intern(env, CString::new(name).unwrap().as_ptr())
+    }
 }
 
 #[no_mangle]
@@ -22,7 +25,8 @@ pub extern "C" fn make_function(env: *mut EmacsEnv,
                                 -> *mut EmacsVal {
     let doc = CString::new(doc).unwrap().as_ptr();
     unsafe {
-        (*env).make_function.unwrap()(env, min_args, max_args, f, doc, user_ptr)
+        let make_function = (*env).make_function.unwrap();
+        make_function(env, min_args, max_args, f, doc, user_ptr)
     }
 }
 
@@ -31,18 +35,27 @@ pub extern "C" fn make_emacs_string<T: Into<Vec<u8>>>(env: *mut EmacsEnv,
                                                       -> *mut EmacsVal {
     let c_string = CString::new(string).unwrap().as_ptr();
     let strlen = unsafe { libc::strlen(c_string) as i64 };
-    unsafe { (*env).make_string.unwrap()(env, c_string, strlen) }
+    unsafe {
+        let make_string = (*env).make_string.unwrap();
+        make_string(env, c_string, strlen)
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn get_environment(ert: *mut EmacsRT) -> *mut EmacsEnv {
-    unsafe { (*ert).get_environment.unwrap()(ert) }
+    unsafe {
+        let get_env = (*ert).get_environment.unwrap();
+        get_env(ert)
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn intern_symbol(env: *mut EmacsEnv, name: String)
                                 -> *mut EmacsVal {
-    unsafe { (*env).intern.unwrap()(env, CString::new(name).unwrap().as_ptr()) }
+    unsafe {
+        let intern = (*env).intern.unwrap();
+        intern (env, CString::new(name).unwrap().as_ptr())
+    }
 }
 
 #[no_mangle]
@@ -52,17 +65,24 @@ pub extern "C" fn bind_function(env: *mut EmacsEnv,
     let qfset = find_function(env, "fset".to_string());
     let qsym = intern_symbol(env, name);
     let args = [qsym, sfun].as_mut_ptr();
-    unsafe { (*env).funcall.unwrap()(env, qfset, 2, args) };
+    unsafe {
+        let funcall = (*env).funcall.unwrap();
+        funcall(env, qfset, 2, args)
+    };
 }
 
 #[no_mangle]
 pub extern "C" fn provide(env: *mut EmacsEnv, feature: String) {
     let feat = unsafe {
-        (*env).intern.unwrap()(env, CString::new(feature).unwrap().as_ptr())
+        let intern = (*env).intern.unwrap();
+        intern (env, CString::new(feature).unwrap().as_ptr())
     };
     let provide = find_function(env, "provide".to_string());
     let args = [feat].as_mut_ptr();
-    unsafe { (*env).funcall.unwrap()(env, provide, 1, args) };
+    unsafe {
+        let funcall = (*env).funcall.unwrap();
+        funcall(env, provide, 1, args)
+    };
 }
 
 
@@ -71,5 +91,8 @@ pub extern "C" fn get_buffer(env: *mut EmacsEnv, buffer: String)
                              -> *mut EmacsVal {
     let get_buffer = find_function(env, "get-buffer".to_string());
     let args = [make_emacs_string(env, buffer)].as_mut_ptr();
-    unsafe { (*env).funcall.unwrap()(env, get_buffer, 1, args) }
+    unsafe {
+        let funcall = (*env).funcall.unwrap();
+        funcall(env, get_buffer, 1, args)
+    }
 }
