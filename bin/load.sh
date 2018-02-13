62902027d60c979fb91f5e27ae64ed3101352812
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2018-02-13 16:39:19 +0700

    Make test & load scripts work in Linux

--- bin/load.sh
+++ bin/load.sh
@@ -2,10 +2,20 @@
 
 # (Re)load test-module into a running Emacs instance.
 
+system=`uname`
+if [[ $system == "Linux" ]]; then
+    ext="so"
+elif [[ $system == "Darwin" ]]; then
+    ext="dylib"
+else
+    echo "Unsupported system: $system"
+    exit 1
+fi
+
 here=`cd $(dirname $BASH_SOURCE); pwd`
 root=`cd $here/..; pwd`
-RS_MODULE=$(find $root -iname '*emacs_rs_module*.dylib' | head -n 1)
-MODULE=$root/target/debug/libtest_module.dylib
+RS_MODULE=$(find $root -iname "*emacs_rs_module*.$ext" | head -n 1)
+MODULE=$root/target/debug/libtest_module.$ext
 
 read -r -d '' expr <<EOF
 (progn
@@ -13,6 +23,8 @@ read -r -d '' expr <<EOF
     (module-load "$RS_MODULE"))
   (rs-module/load "$MODULE"))
 EOF
+
+echo $expr
 
 emacsclient -e "$expr"
 
--- bin/test.sh
+++ bin/test.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 
-# TODO: Use Rust instead of bash to drive the tests.
+# FIX: Use Rust instead of bash to drive the tests.
+
+system=`uname`
+if [[ $system == "Linux" ]]; then
+    ext="so"
+elif [[ $system == "Darwin" ]]; then
+    ext="dylib"
+else
+    echo "Unsupported system: $system"
+    exit 1
+fi
 
 here=`cd $(dirname $BASH_SOURCE); pwd`
 root=`cd $here/..; pwd`
-
 MODULE_DIR=$root/target/debug
 
 # rs-module
-`cd $MODULE_DIR && ln -f -s libemacs_rs_module.dylib rs-module.so`
+`cd $MODULE_DIR && ln -f -s libemacs_rs_module.$ext rs-module.so`
 
 # test-module
-MODULE_ORIG_FILE=libtest_module.dylib
-MODULE_FILE=test-module.so
-`cd $MODULE_DIR && ln -f -s $MODULE_ORIG_FILE $MODULE_FILE`
-MODULE=$MODULE_DIR/$MODULE_FILE
+MODULE_ORIGINAL=libtest_module.$ext
+MODULE_RENAMED=test-module.so
+`cd $MODULE_DIR && ln -f -s $MODULE_ORIGINAL $MODULE_RENAMED`
 
 EMACS=emacs
 
-RUST_BACKTRACE=1 $EMACS -batch -l ert -l $MODULE -l $MODULE_DIR/rs-module.so -l $root/test-module/src/test.el -f ert-run-tests-batch-and-exit
+RUST_BACKTRACE=1 $EMACS -batch -l ert \
+              -l $MODULE_DIR/$MODULE_RENAMED \
+              -l $MODULE_DIR/rs-module.so \
+              -l $root/test-module/src/test.el \
+              -f ert-run-tests-batch-and-exit
