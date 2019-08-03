00af9ebb63acab181b1342930785866958a067d2
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2019-08-03 20:18:30 +0700

    Move Env into its own submodule

---
+++ src/env.rs
@@ -0,0 +1,85 @@
+use std::{cell::RefCell, ffi::CString};
+
+use emacs_module::{emacs_runtime, emacs_env, emacs_value};
+
+use crate::{Value, Result};
+
+/// Main point of interaction with the Lisp runtime.
+#[derive(Debug)]
+pub struct Env {
+    pub(crate) raw: *mut emacs_env,
+    /// Raw values "rooted" during the lifetime of this `Env`.
+    pub(crate) protected: RefCell<Vec<emacs_value>>,
+}
+
+/// Public APIs.
+impl Env {
+    #[doc(hidden)]
+    pub unsafe fn new(raw: *mut emacs_env) -> Self {
+        let protected = RefCell::new(vec![]);
+        Self { raw, protected }
+    }
+
+    #[doc(hidden)]
+    pub unsafe fn from_runtime(runtime: *mut emacs_runtime) -> Self {
+        let get_env = (*runtime).get_environment.expect("Cannot get Emacs environment");
+        let raw = get_env(runtime);
+        Self::new(raw)
+    }
+
+    #[doc(hidden)]
+    pub fn raw(&self) -> *mut emacs_env {
+        self.raw
+    }
+
+    pub fn intern(&self, name: &str) -> Result<Value<'_>> {
+        raw_call_value!(self, intern, CString::new(name)?.as_ptr())
+    }
+
+    // TODO: Return an enum?
+    pub fn type_of(&self, value: Value<'_>) -> Result<Value<'_>> {
+        raw_call_value!(self, type_of, value.raw)
+    }
+
+    // TODO: Add a convenient macro?
+    pub fn call(&self, name: &str, args: &[Value<'_>]) -> Result<Value<'_>> {
+        let symbol = self.intern(name)?;
+        // XXX Hmm
+        let mut args: Vec<emacs_value> = args.iter().map(|v| v.raw).collect();
+        raw_call_value!(self, funcall, symbol.raw, args.len() as isize, args.as_mut_ptr())
+    }
+
+    #[deprecated(since = "0.10.0", note = "Please use `value.is_not_nil()` instead")]
+    pub fn is_not_nil(&self, value: Value<'_>) -> bool {
+        raw_call_no_exit!(self, is_not_nil, value.raw)
+    }
+
+    #[deprecated(since = "0.10.0", note = "Please use `value1.eq(value2)` instead")]
+    pub fn eq(&self, a: Value<'_>, b: Value<'_>) -> bool {
+        raw_call_no_exit!(self, eq, a.raw, b.raw)
+    }
+
+    pub fn list(&self, args: &[Value<'_>]) -> Result<Value<'_>> {
+        self.call("list", args)
+    }
+
+    pub fn provide(&self, name: &str) -> Result<Value<'_>> {
+        let name = self.intern(name)?;
+        call_lisp!(self, "provide", name)
+    }
+
+    pub fn message<T: AsRef<str>>(&self, text: T) -> Result<Value<'_>> {
+        call_lisp!(self, "message", text.as_ref())
+    }
+}
+
+// TODO: Add tests to make sure the protected values are not leaked.
+impl Drop for Env {
+    fn drop(&mut self) {
+        #[cfg(build = "debug")]
+        println!("Unrooting {} values protected by {:?}", self.protected.borrow().len(), self);
+        for raw in self.protected.borrow().iter() {
+            raw_call_no_exit!(self, free_global_ref, *raw);
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,6 @@
 //! [Examples]: https://github.com/ubolonton/emacs-rs-examples/
 
 use std::cell::{RefCell, Ref, RefMut};
-use std::ffi::CString;
 use std::convert::TryInto;
 
 #[doc(inline)]
@@ -38,11 +37,15 @@ use raw::*;
 pub use failure::{self, Error};
 
 #[doc(inline)]
-pub use self::error::{ErrorKind, Result, ResultExt};
-pub use self::func::CallEnv;
+pub use self::{
+    env::Env,
+    func::CallEnv,
+    error::{ErrorKind, Result, ResultExt},
+};
 
 #[macro_use]
 mod macros;
+mod env;
 mod convert;
 
 #[doc(hidden)]
@@ -66,14 +69,6 @@ pub mod raw {
 pub mod deps {
     pub use ctor;
     pub use lazy_static;
-}
-
-/// Main point of interaction with the Lisp runtime.
-#[derive(Debug)]
-pub struct Env {
-    pub(crate) raw: *mut emacs_env,
-    /// Raw values "rooted" during the lifetime of this `Env`.
-    pub(crate) protected: RefCell<Vec<emacs_value>>,
 }
 
 /// A type that represents Lisp values.
@@ -155,78 +150,6 @@ pub trait Transfer: Sized + 'static {
 
     // TODO: Consider using a wrapper struct to carry the type info, to enable better runtime
     // reporting of type error (and to enable something like `rs-module/type-of`).
-}
-
-/// Public APIs.
-impl Env {
-    #[doc(hidden)]
-    pub unsafe fn new(raw: *mut emacs_env) -> Self {
-        let protected = RefCell::new(vec![]);
-        Self { raw, protected }
-    }
-
-    #[doc(hidden)]
-    pub unsafe fn from_runtime(runtime: *mut emacs_runtime) -> Self {
-        let get_env = (*runtime).get_environment.expect("Cannot get Emacs environment");
-        let raw = get_env(runtime);
-        Self::new(raw)
-    }
-
-    #[doc(hidden)]
-    pub fn raw(&self) -> *mut emacs_env {
-        self.raw
-    }
-
-    pub fn intern(&self, name: &str) -> Result<Value<'_>> {
-        raw_call_value!(self, intern, CString::new(name)?.as_ptr())
-    }
-
-    // TODO: Return an enum?
-    pub fn type_of(&self, value: Value<'_>) -> Result<Value<'_>> {
-        raw_call_value!(self, type_of, value.raw)
-    }
-
-    // TODO: Add a convenient macro?
-    pub fn call(&self, name: &str, args: &[Value<'_>]) -> Result<Value<'_>> {
-        let symbol = self.intern(name)?;
-        // XXX Hmm
-        let mut args: Vec<emacs_value> = args.iter().map(|v| v.raw).collect();
-        raw_call_value!(self, funcall, symbol.raw, args.len() as isize, args.as_mut_ptr())
-    }
-
-    #[deprecated(since = "0.10.0", note = "Please use `value.is_not_nil()` instead")]
-    pub fn is_not_nil(&self, value: Value<'_>) -> bool {
-        raw_call_no_exit!(self, is_not_nil, value.raw)
-    }
-
-    #[deprecated(since = "0.10.0", note = "Please use `value1.eq(value2)` instead")]
-    pub fn eq(&self, a: Value<'_>, b: Value<'_>) -> bool {
-        raw_call_no_exit!(self, eq, a.raw, b.raw)
-    }
-
-    pub fn list(&self, args: &[Value<'_>]) -> Result<Value<'_>> {
-        self.call("list", args)
-    }
-
-    pub fn provide(&self, name: &str) -> Result<Value<'_>> {
-        let name = self.intern(name)?;
-        call_lisp!(self, "provide", name)
-    }
-
-    pub fn message<T: AsRef<str>>(&self, text: T) -> Result<Value<'_>> {
-        call_lisp!(self, "message", text.as_ref())
-    }
-}
-
-// TODO: Add tests to make sure the protected values are not leaked.
-impl Drop for Env {
-    fn drop(&mut self) {
-        #[cfg(build = "debug")]
-        println!("Unrooting {} values protected by {:?}", self.protected.borrow().len(), self);
-        for raw in self.protected.borrow().iter() {
-            raw_call_no_exit!(self, free_global_ref, *raw);
-        }
-    }
 }
 
 impl<'e> Value<'e> {
