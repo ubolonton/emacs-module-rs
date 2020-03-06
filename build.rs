4c05185ee4e3d85f706a62f498369737d0520b20
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2020-03-06 22:53:33 +0700

    Improve error message on old unsupported Rust versions
    This fixes #25

---
+++ build.rs
@@ -0,0 +1,12 @@
+use std::process::exit;
+
+extern crate rustc_version;
+use rustc_version::{version, Version};
+
+fn main() {
+    let version = version().unwrap();
+    if version < Version::parse("1.38.0").unwrap() {
+        eprintln!("emacs-module-rs requires rustc 1.38.0 or newer, got {}", version);
+        exit(1);
+    }
+}
