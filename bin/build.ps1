711f5faf189e0399b4472c6baec8ac344736456d
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2024-03-15 22:35:47 +0700

    build: Fix Powershell scripts

--- bin/build.ps1
+++ bin/build.ps1
@@ -1,21 +1,22 @@
 $here = $PSScriptRoot
 $project_root = (Get-Item $here).Parent.FullName
-
 $target = $args[0]
-if ($target -eq "release") {
-    $extra = "--release"
-} else {
-    $target = "debug"
-    $extra = ""
-}
-
-$module_dir = "$project_root\target\$target"
 
 Push-Location $project_root
-
-cargo build --all $extra
-
-Pop-Location
+try {
+    switch ($target) {
+        'release' {
+            $module_dir = "$project_root\target\release"
+            cargo build --all --release
+        }
+        default {
+            $module_dir = "$project_root\target\debug"
+            cargo build --all
+        }
+    }
+} finally {
+    Pop-Location
+}
 
 Copy-Item $module_dir\emacs_rs_module.dll $module_dir\rs-module.dll
 Copy-Item $module_dir\test_module.dll $module_dir\t.dll
